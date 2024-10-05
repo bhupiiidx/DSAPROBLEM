fdfd24b09828547076339d6a3238848bf6313595
bhupiiidx <bhupender81238@gmail.com>
2024-10-05 11:08:16 +0530

    Programming-Sheet-150-Basic-added

--- LeetCode/LeetCode-1-E/Solution-Optimal.js
+++ LeetCode/LeetCode-1-E/Solution-Optimal.js
@@ -8,6 +8,9 @@ var twoSum = function(nums, target) {
     let dict = {}
     let indices = []
     for(let i = 0; i<nums.length;i++){
+        // check if the difference between target and current element is in the dictionary
+        // if it is, that means we have found two numbers that add up to the target
+        // break the loop and return the indices of the two numbers
         if((dict[target - nums[i]]) !== undefined){
             indices =  [dict[target - nums[i]],i]
             break;
